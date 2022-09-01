cc68634baf7296c8f20e0fc8840087d2ddd56f88
Hari <harikesh.singh@gmail.com>
2022-09-01 09:07:15 +0530

    assignment 8 and 9

---
+++ Assignment8.py
@@ -0,0 +1,90 @@
+#1. Write a python script to print MySirG 5 times on the screen
+i=1
+while i<=5:
+    print("MySirG",sep='\n')
+    i+=1
+print
+
+#2. Write a python script to print first 10 natural numbers
+i=1
+while i<=10:
+    print(i,sep='\n')
+    i+=1
+print()
+
+#3. Write a python script to print first 10 natural numbers in reverse order
+
+i=1
+while i<=10:
+    print(11-i,sep='\n')
+    i+=1
+print()
+
+#or
+
+i=1
+a=10
+while i<=10:
+    print(a,sep='\n')
+    a-=1
+    i+=1
+print()
+
+#4. Write a python script to print first 10 odd natural numbers
+
+i=0
+while i<10:
+    print(2*i+1, sep='\n')
+    i+=1
+print()
+
+#5. Write a python script to print first 10 odd natural numbers in reverse order
+
+i=10
+while 0<i<11:
+    print(2*i-1, sep='\n')
+    i-=1
+print()
+
+#6. Write a python script to print first 10 even natural numbers
+
+
+i=1
+while i<=10:
+    print(2*i)
+    i+=1
+print()
+
+
+#7. Write a python script to print first 10 even natural numbers in reverse order
+i=10
+while 0<i<=10:
+    print(2*i)
+    i-=1
+print()
+
+#8. Write a python script to print squares of first 10 natural numbers
+i=1
+while i<=10:
+    print(i*i)
+    i+=1
+print()
+
+#9. Write a python script to print cubes of first 10 natural numbers
+
+i=1
+while i<=10:
+    print(i**3)
+    i+=1
+print()
+
+#10. Write a python script to print first 10 multiples of 5
+
+i=1
+while i<=10:
+    print(5*i)
+    i+=1
+print()
+
+
+
---
+++ Assignment9.py
@@ -0,0 +1,90 @@
+#1. Write a python script to print MySirG N times on the screen
+
+n=int(input("enter the value of n: "))
+i=1
+while i<=n:
+    print("MySirG",sep='\n')
+    i+=1
+print
+
+#2. Write a python script to print first N natural numbers
+
+n=int(input("enter the value of n: "))
+i=1
+while i<=n:
+    print(i,sep='\n')
+    i+=1
+print()
+
+#3. Write a python script to print first N natural numbers in reverse order
+
+n=int(input("enter the value of n: "))
+i=0
+while 0<=i<n:
+    print(n-i)
+    i+=1
+print()
+
+#4. Write a python script to print first N odd natural numbers
+
+n=int(input("enter the value of n: "))
+i=0
+while i<n:
+    print(2*i+1, sep='\n')
+    i+=1
+print()
+
+#5. Write a python script to print first N odd natural numbers in reverse order
+n=int(input("Enter the value of n: "))
+i=n
+while 0<i<=n:
+    print(2*i-1)
+    i-=1
+print()
+
+#6. Write a python script to print first N even natural numbers
+
+n=int(input("Enter the value of n: "))
+i=1
+while i<=n:
+    print(2*i)
+    i+=1
+print()
+
+#7. Write a python script to print first N even natural numbers in reverse order
+
+n=int(input("Enter the value of n: "))
+i=n
+while 0<i<=n:
+    print(2*i)
+    i-=1
+print()
+
+#8. Write a python script to print squares of first N natural numbers
+
+n=int(input("Enter the value of n: "))
+i=1
+while i<=n:
+    print(i*i)
+    i+=1
+print()
+
+
+#9. Write a python script to print cubes of first N natural numbers
+
+n=int(input("Enter the value of n: "))
+i=1
+while i<=n:
+    print(i**3)
+    i+=1
+print()
+
+
+#10. Write a python script to print first 10 multiples of N
+
+n=int(input("Enter the value of n: "))
+i=1
+while i<=n:
+    print(5*i)
+    i+=1
+print()
